2df07b767f39dc69a972ba9947b5aabffee65349
Yutts <huynhdong2811@gmail.com>
2021-11-11 11:21:25 +0700

    hoan thanh

--- source.cpp
+++ source.cpp
@@ -13,6 +13,14 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+		if(phepToan=='+'){
+		int kq = tong(a,b);
+		cout<<kq;
+	}
 	system("pause");
 	return 0;
+}
+
+int tong(int a, int b){
+	return a+b;
 }
